b66c428e9ed10ffcf4d33fd2428976c6cf26c253
russellhimes <william.himes@richmond.edu>
2021-09-17 18:17:30 -0400

    Adding Header, Vue Script, and Vue CSS components to App page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
+      <the-header />
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+export default {
+  components: { TheHeader },
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="header">
+      <h1>Stock Picker Game</h1>
+      <span id="login" v-if="$store.state.username">
+          <p v-if="$store.state.username" id="username">{{username}}</p>
+          <routerLink id="logoutLink" to="logout">Log Out</routerLink>
+      </span>
+      <router-link v-else id="loginLink" to="Login">Log In</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#header {
+    max-height: 10vh;
+    width: 100vh;
+    display: flex;
+}
+</style>

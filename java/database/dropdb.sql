239a1ec482fd0f19b7bded0a4cce6beda0cd4742
Kailey Waal <15kwaal@gmail.com>
2021-09-18 10:50:46 -0600

    minor fixes to schema

--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -5,7 +5,7 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'virtual_stock_market';
+WHERE datname = 'final_capstone';
 
 DROP DATABASE virtual_stock_market;
 

99ab0c0b5e5395a6cf49bbab0945c340c3528dbe
Kailey Waal <15kwaal@gmail.com>
2021-09-18 15:30:34 -0600

    finished creating player controller

--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -7,7 +7,7 @@ SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
 WHERE datname = 'final_capstone';
 
-DROP DATABASE final_capstone;
+DROP DATABASE IF EXISTS final_capstone;
 
-DROP USER final_capstone_owner;
-DROP USER final_capstone_appuser;
+DROP USER IF EXISTS final_capstone_owner;
+DROP USER IF EXISTS final_capstone_appuser;
--- java/src/main/java/com/techelevator/controller/PlayerController.java
+++ java/src/main/java/com/techelevator/controller/PlayerController.java
@@ -3,10 +3,7 @@ package com.techelevator.controller;
 import com.techelevator.dao.PlayerDao;
 import com.techelevator.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,31 @@ public class PlayerController {
     @RequestMapping(path="/players", method = RequestMethod.GET)
     public List<Player> getAllPlayers() {
         return playerDao.getAllPlayers();
+    }
+
+    @RequestMapping(path="/players/game/{game_id}", method = RequestMethod.GET)
+    public List<Player> getPlayersByGameId(@PathVariable int game_id) {
+        return playerDao.getPlayersByGame(game_id);
+    }
+
+    @RequestMapping(path="/players/{id}", method=RequestMethod.GET)
+    public Player getPlayerById(@PathVariable int id) {
+        return playerDao.getPlayerById(id);
+    }
+
+    @RequestMapping(path="/players", method= RequestMethod.POST)
+    public void createPlayer(@RequestBody Player player) {
+        playerDao.createPlayer(player);
+    }
+
+    @RequestMapping(path="/players/{id}", method=RequestMethod.PUT)
+    public void updatePlayer(@RequestBody Player player) {
+        playerDao.updatePlayer(player);
+    }
+
+    @RequestMapping(path="/players/{id}", method=RequestMethod.DELETE)
+    public void deletePlayer(@PathVariable int id) {
+        playerDao.deletePlayer(id);
     }
 
 }

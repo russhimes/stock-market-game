0b189b992c8aa38b1c820e5cbe10c4d9f39c37a4
Grace Malles <gracemalles10@gmail.com>
2021-09-19 12:59:40 -0400

    created portfolio component / nested components into gameboard component

--- vue/src/components/GameBoard.vue
+++ vue/src/components/GameBoard.vue
@@ -1,12 +1,23 @@
 <template>
   <div id = "main ">
+    <the-header></the-header>
       <h1>Welcome to the Game Board!</h1>
-
+      <portfolio-balance></portfolio-balance>
+      <portfolio-holdings></portfolio-holdings>
+      <leader-board></leader-board>
   </div>
 </template>
 
 <script>
+import LeaderBoard from '../views/Leader'
+import PortfolioBalance from '../views/Balance'
+// does header need a corresponding view? 
+import TheHeader from './TheHeader.vue'
+import PortfolioHoldings from '../views/Portfolio'
+import PortfolioHoldings from './PortfolioHoldings.vue'
+
 export default {
+  components: { TheHeader, PortfolioBalance, LeaderBoar, PortfolioHoldings },
 
 }
 </script>
---
+++ vue/src/views/Portfolio.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+import PortfolioHoldings from '../components/PortfolioHoldings.vue'
+export default {
+    components: { PortfolioHoldings },
+}
+</script>
+
+<style>
+
+</style>

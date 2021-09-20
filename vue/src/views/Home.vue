25b6c9430b64ec3c4c6c7cc26cc40bbf1da36902
russellhimes <william.himes@richmond.edu>
2021-09-20 09:57:23 -0400

    minor home page updates

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,9 +2,14 @@
   <div class="home">
     <p v-if="!$store.state.user.username">You must be authenticated to see this</p>
     <div v-else>
+      <h2>Active Games</h2>
       <!--Discuss during team stand-up, we need to be able to get games by user,
       and we need to be able to assign games a "pending/accepted/rejected status" -->
       <game-card v-for="game in $store.state.games" v-bind:key="game.id" v-bind:game="game"/>
+      <h2>Pending Invites</h2>
+      <!--Pending Games-->
+      <h2>Rejected Games</h2>
+      <!--RejectedGames-->
     </div>
   </div>
 </template>

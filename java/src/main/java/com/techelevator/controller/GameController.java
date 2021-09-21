ae8424fdf13af51acb114c060ea03086da622d5d
Alison Walker <alisonnicole.23@gmail.com>
2021-09-21 09:58:56 -0500

    Authentication

--- java/src/main/java/com/techelevator/controller/GameController.java
+++ java/src/main/java/com/techelevator/controller/GameController.java
@@ -3,12 +3,15 @@ package com.techelevator.controller;
 import com.techelevator.dao.GameDao;
 import com.techelevator.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
 @CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class GameController {
 
     @Autowired

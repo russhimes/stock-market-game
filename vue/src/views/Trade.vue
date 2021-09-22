b63aff2b39a0471852f01d38e1f9103a71f205c1
Grace Malles <gracemalles10@gmail.com>
2021-09-22 11:41:27 -0400

    almost forgot to make trade.vue - ADDED

---
+++ vue/src/views/Trade.vue
@@ -0,0 +1,15 @@
+<template>
+  <trade-stocks></trade-stocks>
+</template>
+
+<script>
+import TradeStocks from '../components/TradeStocks.vue'
+export default {
+  components: { TradeStocks },
+
+}
+</script>
+
+<style>
+
+</style>

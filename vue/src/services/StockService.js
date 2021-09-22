c42b081dd50b10b5da3c97c59a7fb25d4f85f712
Grace Malles <gracemalles10@gmail.com>
2021-09-22 16:17:07 -0400

    Minor frontend changes

--- vue/src/services/StockService.js
+++ vue/src/services/StockService.js
@@ -7,12 +7,9 @@ export default{
     },
     getStockInfo(ticker){
         return axios.get(`/stockinfo/${ticker}`);
-<<<<<<< HEAD:vue/src/services/TradeStockService.js
-=======
     },
     getPlayerStocks(playerId) {
         return axios.get(`/player/${playerId}/stocks`);
->>>>>>> c01fe76e90f00401520ffcfc0915017cccf83caa:vue/src/services/StockService.js
     }
 
 

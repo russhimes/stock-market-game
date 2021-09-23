fce5987c8f2eb9ff708b5894091b8f2b14c3403f
Alison Walker <alisonnicole.23@gmail.com>
2021-09-23 11:22:46 -0500

    sell trades validation

--- java/src/main/java/com/techelevator/controller/TradeController.java
+++ java/src/main/java/com/techelevator/controller/TradeController.java
@@ -2,6 +2,7 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.TradeDao;
 import com.techelevator.model.Player;
+import com.techelevator.model.Stock;
 import com.techelevator.model.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +14,14 @@ public class TradeController {
     @Autowired
     private TradeDao tradeDao;
 
-    @RequestMapping(path="/trades", method = RequestMethod.POST)
-    public void buyORSellTrade(@RequestBody Trade trade, @RequestBody Player player) {
-        tradeDao.buyOrSellTrade(trade, player);
+    @RequestMapping(path="/trade/buy", method = RequestMethod.POST)
+    public void buyTrade(@RequestBody Trade trade, @RequestBody Player player) {
+        tradeDao.buyTrade(trade, player);
+    }
+
+    @RequestMapping(path="/trade/sell", method = RequestMethod.POST)
+    public void sellTrade(@RequestBody Trade trade, @RequestBody Stock stock) {
+        tradeDao.sellTrade(trade, stock);
     }
 
 }
--- java/src/main/java/com/techelevator/dao/TradeDao.java
+++ java/src/main/java/com/techelevator/dao/TradeDao.java
@@ -1,10 +1,13 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Player;
+import com.techelevator.model.Stock;
 import com.techelevator.model.Trade;
 
 public interface TradeDao {
 
-    void buyOrSellTrade(Trade trade, Player player);
+    void buyTrade(Trade trade, Player player);
+
+    void sellTrade(Trade trade, Stock stock);
 
 }

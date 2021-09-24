2abc0b655efc6dcf134f971d19ae63cd3f122fff
Alison Walker <alisonnicole.23@gmail.com>
2021-09-23 21:45:50 -0500

    Leaderboard sorting on front end

--- vue/src/components/LeaderBoard.vue
+++ vue/src/components/LeaderBoard.vue
@@ -1,8 +1,10 @@
 <template>
 <div class = "LeaderBoard">
-  <h3 class="title">Leader Board!</h3>
+  <h3 class="title">Leaderboard!</h3>
   <p class="body">This is where we will show the stats of the leaderboard!</p>
-
+  <div v-for="player in playerList" v-bind:key="player.id">
+    <div>{{player.id}} : {{player.availableFunds}}</div>
+  </div>
   <!-- <div class='playerCard' v-for='player in playerList'>
     {{ player.name }}
   </div> -->
@@ -14,22 +16,35 @@ import playerService from '../services/PlayerService.js'
 export default {
     data(){
       return {
-        playerList: []
+        playerList: [],
+        sorted : []
       }
     },
     created(){
       // first thing that happens when component loads 
       // taking data returned from getAllPlayers() and saving into our playerList
+      
+      //since this is the leaderboard page and the only use will be sorting, I just implemented it in the created 
+      //portion, will need to do some work to get player names to show, and maybe stocks owned/shares owned
       playerService.getAllPlayers().then(
           (response) => {
             this.playerList = response.data;
+            this.playerlist = this.playerList.sort((a,b) => {
+              return b.availableFunds - a.availableFunds
+            })
           }
       ).catch(
         (error) => console.log(error)
       );
 
-    }
-};
+    },
+    computed: {
+      // filteredList(){
+      //   if()
+      // }
+
+  }
+}
 
 </script>
 

9be4dff6c66856337eb554a9981894ffa32e5b2f
Grace Malles <gracemalles10@gmail.com>
2021-09-24 09:51:57 -0400

    created timer component/ view

---
+++ vue/src/views/Timer.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <countdown-timer></countdown-timer>
+  </div>
+</template>
+
+<script>
+import CountdownTimer from '../components/CountdownTimer.vue'
+export default {
+  components: { CountdownTimer },
+
+}
+</script>
+
+<style>
+
+</style>

3416b44cd317cea450483a8433088ade3859bbc7
Grace Malles <gracemalles10@gmail.com>
2021-09-24 12:26:16 -0400

    refactored timer

--- vue/src/components/CountdownTimer.vue
+++ vue/src/components/CountdownTimer.vue
@@ -9,10 +9,10 @@
                   <td colspan="4" class="info">Game Countdown</td>
               </tr>
               <tr>
-                  <td id= "days">120</td>
-                  <td id= "hours">4</td>
-                  <td id= "minutes">12</td>
-                  <td id= "seconds">22</td>
+                  <td id= "days">{{displayDays}}</td>
+                  <td id= "hours">{{displayHours}}</td>
+                  <td id= "minutes">{{displayMinutes}}</td>
+                  <td id= "seconds">{{displaySeconds}}</td>
               </tr>
               <tr>
                   <td>Days</td>
@@ -30,19 +30,63 @@
 
 <script>
 export default {
+    data: () => ({
+        displayDays: 0,
+        displayHours: 0,
+        displayMinutes: 0,
+        displaySeconds: 0
+    }),
 
     computed: {
-         Countdown() {
-        let now = new Date()
-        // inside of Date will be the date that the game ends 
-        //let eventDate = new Date();
-        return now;
-        
-
+        // values in milisec
+        _seconds: () => 1000,
+        _minutes() {
+            return this._seconds * 60;
+        },
+        _hours(){
+            return this._minutes * 60;
+        },
+        days(){
+            return this._hours * 24;
+        }
     },
-    
-}
-}
+    mounted() {
+      //  this.showRemaining();
+    },
+
+    methods: {
+        showRemaining(){
+            const timer = setInterval(() => {
+                const now = new Date();
+                const end = new Date(2022, 4, 22, 10, 10, 10, 10); // placeholder to test 
+                const distance = end.getTime() - now.getTime();
+
+                // when time is up
+                if(distance < 0){
+                   //stop timer
+                   clearInterval(timer);
+                   return
+
+                }
+                // calculate values from milisec 
+                const days = Math.floor((distance / this._days));
+                const hours = Math.floor((distance % this._days) / this._hours);
+                const minutes = Math.floor((distance % this.hours) / this._minutes);
+                const seconds = Math.floor((distance % this.minutes) / this.seconds);
+                // adding 0 to time 
+                this.displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+                this.displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+                this. displayHours = hours <  10 ? "0" + hours : hours;
+                this.displayDays = days <  10 ? "0" + days : days;
+            }, 1000);
+        }
+    }
+};
+
+
+
+
+      
 </script>
 
 <style>

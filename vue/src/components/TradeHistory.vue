34a96d54fa0b0c47066632c72c10a29c7258b150
Grace Malles <gracemalles10@gmail.com>
2021-09-28 17:29:08 -0400

    styling trade history

--- vue/src/components/TradeHistory.vue
+++ vue/src/components/TradeHistory.vue
@@ -1,18 +1,19 @@
 <template>
-<div class="history">
+<div class="historyContainer">
   <h2>Trade History</h2>
-  <div v-for="trade in tradeHistory" v-bind:key="trade.id">
-  <table>
-      <tr>
-          <th>Date</th>
+ <table>
+     <tr>
+         <th>Date</th>
           <th>Stock</th>
           <th>Shares</th>
           <th>Price</th>
           <th>Position</th>
-      </tr>
-      <tr>
+     </tr>
+ </table>
+  <div class= "history" v-for="trade in tradeHistory" v-bind:key="trade.id">
+  <table>
+      <tr class = "row">
           <td>{{trade.date}} <br>{{trade.time }} </td>
-        
           <td>{{trade.stock_name }}</td>
           <td>{{trade.shares_traded }}</td>
           <td>{{trade.price }}</td>
@@ -42,12 +43,39 @@ export default {
 </script>
 
 <style scoped>
+
+.historyContainer {
+display: flex;
+        flex-direction: column;
+        padding: 3rem;
+        margin: var(--padding);
+        background-color: var(--color-lighter);
+        border-radius: var(--border-radius);
+        color: var(--background-color);
+        width: 60vw;
+        justify-content: center;
+        
+}
+.history{
+    display: flex;
+    max-height: 50vh;
+    overflow-y: scroll;
+    width: 100%;
+}
+.row{
+    font-size: 12px;
+    
+}
+
 div {
-    color: black;
+    color: white;
 }
 td {
-    font-size: 10px;
+    font-size: 12px;
     text-align: center;
-   
 }
+
+
+   
+
 </style>
